d6511f7feeb96dd37b418dae56330ed7848cd342
Fedex832 <philippe.mariel832@gmail.com>
2020-09-23 11:28:57 +0200

    feat(TP1): exercise 1 to 9

---
+++ variable/index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/**
+* Exercies1
+*/
+
+// My Digital School Forever
+
+/**
+* Exercies2
+*/
+
+/* My Digital School Forever every day */
+
+/**
+* Exercies3
+*/
+
+var string = "42";
+var number = 42;
+var float = 42.0;
+var boolean = true;
+var array = [];
+var date = Date.now();
+var objet = {};
+var undefined;
+var test = null;
+
+/**
+* Exercies4
+*/
+
+var my42count = "quarante-deux";
+var count = my42count.length;
+
+/**
+* Exercies5
+*/
+
+/**
+* Exercies6
+*/
+
+var myArray42 = ['q', 'u', 'a', 'r', 'a', 'n', 't', 'e', '-', 'd', 'e', 'u', 'x'];
+
+/**
+* Exercies7
+*/
+
+var myArray42Len = myArray42.length;
+
+/**
+* Exercies8
+*/
+
+var maConc = myArray42.join('') + ' La grande réponse sur la vie, l’univers et le reste !'
+console.log(maConc);
+
+/**
+* Exercies9
+*/
+
+Math.floor(Math.random() * 100);
